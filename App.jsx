426c1a463317cac809f1dcfbbff590ebfecc67ad
vanejan <vjan888@gmail.com>
2024-01-01 23:46:59 -0800

    Changed title

--- App.jsx
+++ App.jsx
@@ -2,6 +2,13 @@ import { useEffect, useState } from "react"
 import "./styles.css"
 import { Route, Routes } from "react-router-dom";
 
+/* Import roboto font */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 import TodoPage from "./pages/TodoPage/TodoPage"
 
 export default function App() {
