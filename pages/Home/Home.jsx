c09d2c7393880279955d70468550586875e33f26
vanejan <vjan888@gmail.com>
2024-01-03 23:20:16 -0800

    Added more styling

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -2,11 +2,20 @@ import { useState } from 'react';
 import Calendar from 'react-calendar';
 import s from "./Home.module.css";
 import "./react-calendar.css";
+import {useNavigate} from "react-router-dom";
 
 export default function Home() {
     const [date, setDate] = useState(new Date());
+    const navigate = useNavigate();
+
 
     return (
-        <Calendar onChange={setDate} value={date} />
+        <div>
+            <h2>Calendar View</h2>
+            <Calendar 
+                onChange={setDate} 
+                value={date} 
+            />
+        </div>
     );
 }
